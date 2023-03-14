d4abe7920503847bfe1d6e44bbdbf1c1d2339b42
Tamás Kozák <minikozi99@gmail.com>
2023-03-14 15:35:04 +0100

    home and about components

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,7 +1,19 @@
 export default function About() {
   return (
-    <div className="about">
-      <h1>About</h1>
-    </div>
+    <section id="about">
+      <div className="px-32">
+        <h2 className="text-3xl font-bold">About</h2>
+        <p className="pt-3 pb-6">
+          I am an Information Technology student in Venlo, currently looking for{" "}
+          <br></br> internship opportunities in the Netherlands starting from
+          September 2023.{" "}
+        </p>
+        <p className="pt-3 pb-12">
+          I have worked on numerous projects throughout my education, both for{" "}
+          <br></br> the university and for customers. I am happy to answer if
+          you have any questions.
+        </p>
+      </div>
+    </section>
   );
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,13 +2,12 @@ import React from "react";
 
 export default function Home() {
   return (
-    <section className="px-32 py-64" id="home">
-      <div className="">
+    <section className="px-32 py-96" id="home">
+      <div className="min-w-{500px}">
         <p className="text-black text-3xl w-full z-50">
           Hello, I’m Tamás Kozák.
         </p>
         <p className="text-gray text-3xl">Software Engineer</p>
-        <h1 className="pt-10 pl-[90%] text-3xl font-bold">↓</h1>
       </div>
     </section>
   );
